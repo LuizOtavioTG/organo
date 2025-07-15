77ec7a610c1edf94f9d46cbbb031323aa28757d7
LuizOtavioTG <bc.luizo@gmail.com>
2025-07-15 09:49:08 -0300

    Add: Estrutura inicial da continuação do projeto Organo

--- src/app/componentes/livro/livro.component.ts
+++ src/app/componentes/livro/livro.component.ts
@@ -1,19 +1,24 @@
-import { Component, input } from '@angular/core';
-import { Livro } from './livro';
 import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 
+import { Livro } from './livro';
+import { BotaoComponent } from '../botao/botao.component';
 
 @Component({
   selector: 'app-livro',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BotaoComponent
+  ],
   templateUrl: './livro.component.html',
   styleUrl: './livro.component.css'
 })
 export class LivroComponent {
 
-  livro =  input.required<Livro>();
+  livro = input.required<Livro>();
 
-  alternarFavorito(){
+  alternarFavorito() {
     this.livro().favorito = !this.livro().favorito;
-  };
+  }
+
 }
