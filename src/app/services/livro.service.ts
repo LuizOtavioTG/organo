2f91d691a4ea7b94516983d38dc89e88b0f0b667
LuizOtavioTG <bc.luizo@gmail.com>
2025-07-16 21:22:03 -0300

    Feat: Criando service para obter livros do backend

---
+++ src/app/services/livro.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { GeneroLiterario, Livro } from '../componentes/livro/livro';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+  private API_URL = "http://localhost:3001/livros"
+
+  generos: GeneroLiterario[] = [
+    { id: 'romance', value: 'Romance' },
+    { id: 'misterio', value: 'Mistério' },
+    { id: 'fantasia', value: 'Fantasia' },
+    { id: 'ficcao-cientifica', value: 'Ficção Científica' },
+    { id: 'tecnicos', value: 'Técnicos' }
+  ];
+
+  constructor(private httpClient: HttpClient) { }
+
+  obterLivros(): Observable<Livro[]> {
+    return this.httpClient.get<Livro[]>(this.API_URL);
+  }
+  organizarLivrosPorGenero(): Observable<Map<string, Livro[]>> {
+    return this.obterLivros().pipe(
+      map((livros: Livro[]) => {
+        const livrosPorGenero = new Map<string, Livro[]>();
+
+        livros.forEach((livro: Livro) => {
+          const generoId = typeof livro.genero === 'string' ? livro.genero : livro.genero?.id;
+
+          if (generoId) {
+            if (!livrosPorGenero.has(generoId)) {
+              livrosPorGenero.set(generoId, [])
+            }
+            livrosPorGenero.get(generoId)?.push(livro)
+          }
+        })
+        return livrosPorGenero
+      })
+    )
+  }
+}
+
